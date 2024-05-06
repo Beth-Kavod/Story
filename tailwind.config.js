8d8551fb8b8755c71037b1d9ebdf1897826e5524
niko-drossos <8she682gsyhaas@protonmail.com>
2024-05-06 13:50:22 -0700

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/*.svelte","./src/**/*.svelte","./src/lib/components/*.svelte"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff5722',
+        secondary: '#2196f3',
+        accent: '#0058ff',
+        background: '#1D232A',
+        error: "#cb0035",
+        info: "#00879a",
+      },
+    },
   },
   plugins: [],
 }
