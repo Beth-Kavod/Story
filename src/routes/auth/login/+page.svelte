0ccdc8f8558298ff1147a9e9a4d2fa6f713c75ee
n8hadden <n8hadden888@gmail.com>
2024-05-06 14:25:11 -0700

    started working on the tailwindcss

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,8 +1,43 @@
 <script>
-  import './styles.css'
+  import '$lib/css/output.css';
 
   import loginUser from '$lib/helpers/loginUser';  
 </script>
+
+<form on:submit={(event) => loginUser(event)} class="w-full max-w-sm mt-12">
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Username
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+        Password
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+    </div>
+  </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3"></div>
+    <div class="md:w-2/3">
+      <button class="shadow bg-accent hover:bg-accent focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
+
+<!--
+
+Gargabe
 
 <form on:submit={(event) => loginUser(event)}>
   <label for="username">
@@ -15,3 +50,5 @@
   </label>
   <button type="submit">Login</button>
 </form>
+
+-->
