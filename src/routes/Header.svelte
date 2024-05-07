25168a9d820c889f472d0751b051cf838145f6e1
niko-drossos <8she682gsyhaas@protonmail.com>
2024-05-07 12:13:38 -0700

    minor css changes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -30,7 +30,7 @@
   </h2>
   
   <label for="search" class="flex w-4/6 bg-white p-2 rounded-lg border border-gray-500">
-    <button class="flex justify-center align-center mr-2">
+    <button class="flex justify-center align-center mx-2">
       <img src={filterIcon} alt="Filter button" class="w-9 h-9">
       {#if $filterCount > 0}
         <p class="text-lg text-center text-gray-500  flex align-center">({$filterCount})</p>
@@ -39,7 +39,7 @@
 
     <input type="text" id="search" placeholder="Search" class="w-full border border-gray-200 px-2 rounded">
     
-    <button on:click={() => {alert("Search works")}}>  <!-- TODO: Make this search -->
+    <button class="ml-2" on:click={() => {alert("Search works")}}>  <!-- TODO: Make this search -->
       <img src={searchIcon} alt="search button" class="w-9 h-9">
     </button>
   </label>
