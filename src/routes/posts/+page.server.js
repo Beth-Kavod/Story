e6404b5c3651f89de21d289634177a4e5172f918
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-05-14 10:37:16 -0700

    added post searching

--- src/routes/posts/+page.server.js
+++ src/routes/posts/+page.server.js
@@ -1,3 +1,5 @@
+import { ApiHostname } from '$env/static/private'
+
 /* export async function load({ params }) {
   const { filename } = params;
   // TODO: Test api routes 
@@ -10,3 +12,36 @@
     throw new Error('Failed to load image');
   }
 } */
+
+export const actions = {
+  searchPosts: async ({ request, cookies }) => {
+    const data = await request.formData()
+
+    let searchQuery = `?`
+
+    if (data.get('title')) searchQuery += `title=${data.get('title')}&`
+    if (data.get('content')) searchQuery += `content=${data.get('content')}&`
+    // TODO: Add more filters
+
+    const fetchPosts = await fetch(`${ApiHostname}/search/posts`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    })
+
+    const response = await fetchPosts.json()
+
+    console.log(response)
+
+    if (!response.success) {
+      return {
+        success: false,
+        message: response.errorMessage
+      }
+    }
+
+    return response
+  }
+}
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,6 +1,22 @@
 <script>
-  export let imageUrl;
+  import { enhance } from '$app/forms';
+  import searchIcon from '$lib/assets/icons/search.svg';
+  export let form
+
+  console.log(form)
 </script>
-<!-- TODO: Not completed -->
+
 <h1>Posts page</h1>
-<!-- <img src="{imageUrl}" alt="Image"> -->
+<form class="flex bg-white p-2 border rounded" method="POST" action="?/searchPosts" use:enhance>
+  <label class="text-black flex items-center mr-2" for="search">Title:</label>
+  <input type="text" id="search" name="title" placeholder="Search" class="w-full border border-gray-200 px-2 rounded text-black">
+  <button type="submit" class="ml-2">
+    <img src={searchIcon} alt="search button" class="w-9 h-9">
+  </button>
+</form>
+<!-- Post container -->
+<div class="mt-4 grid grid-cols-3 w-9/12 gap-4">
+  <div class="border-4 bg-secondary border-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. In, inventore! Est dolore repellendus alias magnam deserunt! Quisquam sapiente incidunt distinctio quae, quia unde, assumenda vitae voluptates fuga, suscipit laborum culpa!</div>
+  <div class="border-4 bg-secondary border-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. In, inventore! Est dolore repellendus alias magnam deserunt! Quisquam sapiente incidunt distinctio quae, quia unde, assumenda vitae voluptates fuga, suscipit laborum culpa!</div>
+  <div class="border-4 bg-secondary border-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. In, inventore! Est dolore repellendus alias magnam deserunt! Quisquam sapiente incidunt distinctio quae, quia unde, assumenda vitae voluptates fuga, suscipit laborum culpa!</div>
+</div>
