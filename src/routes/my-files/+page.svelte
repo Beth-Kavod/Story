d0af55dd271cb8db503b07828bca31f1db62ed9f
n8hadden <n8hadden888@gmail.com>
2024-05-14 13:07:12 -0700

    styled the my-files page

--- src/routes/my-files/+page.svelte
+++ src/routes/my-files/+page.svelte
@@ -1,1 +1,21 @@
-<h1>My Files page</h1>
+<div class="w-screen">
+    <div class="mx-auto w-10/12">
+        <h2 class="mt-8 text-5xl">Images</h2>
+        <hr class="h-px mb-8 mt-4 bg-gray-200 border-1">
+        <div class="container mb-6 flex flex-row">
+            #IMGHOLDER
+        </div>
+
+        <h2 class="text-5xl">Videos</h2>
+        <hr class="h-px mb-8 mt-4 bg-gray-200 border-1">
+        <div class="container mb-6 flex flex-row">
+            #VIDHOLDER
+        </div>
+
+        <h2 class="text-5xl">Audios</h2>
+        <hr class="h-px mb-8 mt-4 bg-gray-200 border-1">
+        <div class="container mb-6 flex flex-row">
+            #AUDIOHOLDER
+        </div>
+    </div>
+</div>
