def5a9ac8fd9b93b5474bce66e51919d96889716
niko-drossos <8she682gsyhaas@protonmail.com>
2024-05-16 13:35:57 -0700

    upload form CSS and added post upload requirements

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Hadden
-Work on uploadForm component to make it look nice.
+Upload post form fields:
+Date
+Title 
+Description (textarea)
+Privacy (Select menu with options: Public, Private, Unlisted)
+Tags (Copy the tag handler from the upload form)
+Also add Images, Videos, and Audios as separate select menus but don't worry about the options yet
 
 Once your done with the form, work on the post page and style the component UserPost.svelte to look like the Figma.
 For the time being you can omit the journal section
--- src/lib/components/UploadFileForm.svelte
+++ src/lib/components/UploadFileForm.svelte
@@ -16,39 +16,39 @@
   }
 </script>
 
-<label for="files{index}" class="mb-2">
+<label for="files{index}" class="mb-2 flex justify-between items-center text-lg">
   Upload:
   <input class="text-sm text-gray-900 rounded cursor-pointer bg-gray-50 focus:outline-none"  id="file{index}" type="file" name="file{index}" required>
 </label>
-<label for="date{index}" class="mb-2">
+<label for="date{index}" class="mb-2 flex justify-between items-center text-lg">
   Date:
-  <input id="date{index}" type="date" name="date{index}" required>
+  <input id="date{index}" type="date" name="date{index}" class="p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded" required>
 </label>
-<label for="title{index}" class="mb-2">
+<label for="title{index}" class="mb-2 flex justify-between items-center text-lg">
   Title:
-  <input id="title{index}" type="text" name="title{index}">
+  <input id="title{index}" type="text" name="title{index}" class="p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded">
 </label>
-<label for="description{index}" class="mb-2">
+<label for="description{index}" class="mb-2 flex justify-between items-center text-lg">
   Description:
-  <input id="description{index}" type="text" name="description{index}">
+  <input id="description{index}" type="text" name="description{index}" class="p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded">
 </label>
-<label for="privacy{index}" class="mb-2">
+<label for="privacy{index}" class="mb-2 flex justify-between items-center text-lg">
   Privacy:
-  <select id="privacy{index}" name="privacy{index}">
+  <select id="privacy{index}" name="privacy{index}" class="p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded">
     <option value="Private">Private</option>
     <option value="Public">Public</option>
     <option value="Unlisted">Unlisted</option>
   </select>
 </label>
-<label for="tags{index}" class="mb-2">
-  Tags:
-  <input list="knownTags" id="tags{index}" type="text">
+<label for="tags{index}" class="mb-2 flex justify-between items-center text-lg">
+  Add Tags:
+  <input list="knownTags" id="tags{index}" type="text" class="mx-1 p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded">
   <datalist id="knownTags">
     <!-- TODO: Add more tags -->
     <option value="tag1">Tag 1</option>
     <option value="tag2">Tag 2</option>
   </datalist>
-  <button class="text-blue-500 bg-slate-500" on:click={event => { addTag(`tags${index}`); event.preventDefault() }}>Add</button>
+  <button class="text-primary font-semibold bg-white p-1 border border-4 border-slate-500 border-l-slate-700 border-t-slate-700 rounded" on:click={event => { addTag(`tags${index}`); event.preventDefault() }}>Add</button>
 </label>
 
 <!-- Hidden input field to store the selectedTags array in FormData for request -->
