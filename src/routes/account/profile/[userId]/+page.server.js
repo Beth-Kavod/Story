0339dbf45aabf91df70003fce9e95f7d3fcd717f
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-05-29 12:19:26 -0700

    Corrected to use ApiHostname

--- src/routes/account/profile/[userId]/+page.server.js
+++ src/routes/account/profile/[userId]/+page.server.js
@@ -1,11 +1,9 @@
-import requireAuth from '$lib/hooks/requireAuth.js';
+import { ApiHostname } from '$env/static/private';
 import { error } from '@sveltejs/kit';
-// TODO: Currently not working, throws 500 error
-// export const load = requireAuth;
 
 export const load = async ({ fetch, params }) => {
   const { userId } = params;
-  const response = await fetch(`http://localhost:3000/user/${userId}`);
+  const response = await fetch(`${ApiHostname}/user/${userId}`);
   const user = await response.json();
 
   if (user.success) {
