d059f40e373ce71ed82cb4cee4928121f1d231d1
Niko-Drossos <8sHe682GSYhaas@protonmail.com>
2024-06-13 20:08:53 +0300

    Fixed voteCount updating client side

--- src/lib/components/VoteControls.svelte
+++ src/lib/components/VoteControls.svelte
@@ -1,6 +1,13 @@
 <script>
   export let origin
 
+  // Template response for the updating of the voteCount
+  let response = {
+    data: {
+      voteCount: undefined
+    }
+  }
+
   async function castVote(vote) {
     const originVote = await fetch(`/api/vote/${origin.type}/${origin.id}`, {
       method: 'POST',
@@ -10,15 +17,14 @@
       body: JSON.stringify({ vote })
     }) 
 
-    const response = await originVote.json()
-
-    console.log(response)
+    // Update the response so that it updates the voteCount
+    response = await originVote.json()
   }
 </script>
 
 <div class="w-16 grid grid-cols-3 grid-rows-1">
   <!-- TODO: Get icons for arrows -->
   <button on:click={() => castVote(false)}>{"<"}</button>
-  <span class="text-center">{origin.voteCount}</span>
+  <span class="text-center">{ response.data.voteCount || origin.voteCount}</span>
   <button on:click={() => castVote(true)}>{">"}</button>
 </div>
